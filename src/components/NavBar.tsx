28e792130ec879bee22d307633b96c9f71cdf266
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-09-12 20:45:31 +0530

    added same effect for menu btn too

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import gsap from "gsap";
 
 const NavBar = () => {
-  const handleHoverIn = () => {
+  const burgerHandleHoverIn = () => {
     const t1 = gsap.timeline();
 
     t1.to(".burger_text_one", {
@@ -29,7 +29,7 @@ const NavBar = () => {
     );
   };
 
-  const handleHoverOut = () => {
+  const burgerHandleHoverOut = () => {
     const t2 = gsap.timeline();
 
     t2.to(".burger_text_two", {
@@ -54,6 +54,40 @@ const NavBar = () => {
     );
   };
 
+  const menuHandleHoverIn = () => {
+    const t1 = gsap.timeline();
+
+    t1.to(".menu_text_one", {
+      y: -12,
+      opacity: 0,
+      duration: 0.1,
+    });
+
+    t1.to(".menu_text_two",{
+      top: 0,
+      fontSize : "25px",
+      duration : 0.1,
+      opacity : 1,
+    })
+  };
+
+  const menuHandleHoverOut =()=>{
+    const t2 = gsap.timeline()
+
+    t2.to(".menu_text_two",{
+      top : 12,
+      duration :  0.1 ,
+      opacity  : 0,
+    })
+
+    t2.to(".menu_text_one",{
+      opacity : 1,
+      duration : 0.1,
+      y: 0
+    })
+
+  }
+
   return (
     <div className="fixed px-12 pt-3  border items-center  bg-[#F5E3CD] uppercase flex left-0 w-full justify-between">
       <div className="text-7xl cursor-pointer hover:scale-106 duration-320 text-[#F91814] transition-transform [-webkit-text-stroke:2.5px_white] font-modak">
@@ -61,8 +95,8 @@ const NavBar = () => {
       </div>
       <div className="flex gap-4 font-mouse items-center">
         <a
-          onMouseLeave={handleHoverOut}
-          onMouseEnter={handleHoverIn}
+          onMouseLeave={burgerHandleHoverOut}
+          onMouseEnter={burgerHandleHoverIn}
           className="burger_btn flex relative items-center hover:scale-107 duration-300 justify-center text-[24px] px-5 py-1 font-light text-[#F5E3CD]  bg-[#F91814] rounded-full "
         >
           <span className="burger_text_one">burgers</span>
@@ -70,8 +104,15 @@ const NavBar = () => {
             burgers
           </span>
         </a>
-        <div className=" flex items-center justify-center gap-1  border-2 border-black/20 rounded-full px-5 py-1">
-          <span className="text-[24px]">menu</span>
+        <div
+        onMouseLeave={menuHandleHoverOut}
+          onMouseEnter={menuHandleHoverIn}
+          className="hover:scale-107 duration-280 hover:border-black flex items-center justify-center gap-1  border-2 border-black/20 rounded-full px-5 py-1"
+        >
+          <div className="text-[24px] flex flex-col relative">
+            <span className="menu_text_one">menu</span>
+            <span className="absolute opacity-0 menu_text_two top-10 ">menu</span>
+          </div>
           <FontAwesomeIcon className="text-[20px]" icon={faBarsStaggered} />
         </div>
       </div>
