0587977cd1a1d45b54a012a6350feb1d69da9b40
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-09-16 20:35:57 +0530

    try to add stroke between cursor images (tail type)

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -7,18 +7,57 @@ import gsap from "gsap";
 
 const Cursor = () => {
   const imagesRef = useRef<HTMLDivElement[]>([]);
+  const linesRef = useRef<HTMLDivElement[]>([]);
+
   const images: string[] = [lettuce, tomato, cheese, meat];
 
   useEffect(() => {
     const cursors = imagesRef.current;
 
-    const cursorMove = (x: any) => {
+    const updateLines = () => {
+      for (let i = 0; i < cursors.length - 1; i++) {
+        const current = cursors[i];
+        const next = cursors[i + 1];
+        const line = linesRef.current[i];
+
+        if (!current || !next || !line) continue;
+
+        const x1 = Number(gsap.getProperty(current, "x"));
+        const x2 = Number(gsap.getProperty(next, "x"));
+
+        const y1 = Number(gsap.getProperty(current, "y"));
+
+        const y2 = Number(gsap.getProperty(next, "y"));
+
+        const sx = x2 - x1;
+        const sy = y2 - y1;
+
+
+        const distance = Math.sqrt(sx * sx + sy*sy)
+        const angle = Math.atan2(sy ,sx) * (180 /Math.PI)
+
+
+        gsap.set(line,{
+          width : distance ,
+          x : x1,
+          y : y1 ,
+          rotation : angle,
+          transformOrigin : "left center"
+        })
+
+
+
+      }
+    };
+
+    const cursorMove = (x: MouseEvent) => {
       cursors.forEach((cursor, index) => {
         gsap.to(cursor, {
           x: x.clientX,
           y: x.clientY,
-          duration: 0 + index * 0.4,
+          duration: index * 0.4,
           ease: "sine.out",
+          // onUpdate: updateLines,
         });
       });
     };
@@ -28,6 +67,17 @@ const Cursor = () => {
 
   return (
     <div className="fixed z-100">
+
+      {images.slice(0, -1).map((_, i) => (
+        <div 
+        className="absolute border h-[4px] bg-black"
+          key={i}
+          ref={(e) => {
+           if(e) linesRef.current[i] = e;
+          }}
+        ></div>
+      ))}
+
       {images.map((image, i) => (
         <div
           key={i}
